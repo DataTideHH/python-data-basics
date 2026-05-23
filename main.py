7a4f88141f93f30ee93230df2f1b290c497460e1
Tobias <tobias.j.wietelmann@gmail.com>
2026-05-23 22:23:48 +0200

    Initial Python data environment setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib
+import sklearn
+
+print("Python:", sys.version)
+print("Executable:", sys.executable)
+print("pandas:", pd.__version__)
+print("numpy:", np.__version__)
+print("matplotlib:", matplotlib.__version__)
+print("scikit-learn:", sklearn.__version__)
